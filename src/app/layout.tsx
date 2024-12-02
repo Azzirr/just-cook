aa7b931e867dc62df2d54355af0539fbbc410efe
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2024-12-02 16:25:12 +0100

    feature/added top navigation, global css header styles, changed main layout, added icons, added tech stack to docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Roboto } from "next/font/google";
+import TopNavigation from "@/components/TopNavigation/TopNavigation";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` antialiased ${roboto.className}`}>
+        {/* To think - do we need top navigation on every page? */}
+        <TopNavigation />
         {children}
       </body>
     </html>
