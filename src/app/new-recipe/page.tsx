8ffa0433a832987ac9f0e2c0311f7432412a720b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-12-12 19:49:11 +0100

    add WIP page for recipe form

---
+++ src/app/new-recipe/page.tsx
@@ -0,0 +1,9 @@
+import { RecipeForm } from "@/components/RecipeForm";
+
+export default function NewRecipePage() {
+  return (
+    <div className="mx-auto max-w-lg p-3">
+      <RecipeForm />
+    </div>
+  );
+}
