af4097e556ac7c69f94366709093b794ed7630ee
Matt <75433152+mproznydev@users.noreply.github.com>
2024-12-16 21:15:18 +0100

    add log in and register server actions (#11)
    * add log in and register server actions
    
    * move schemas higher
    
    * test
    
    * .

---
+++ src/actions/register.ts
@@ -0,0 +1,49 @@
+"use server";
+import bcrypt from "bcryptjs";
+import { db } from "@/db";
+import { Role } from "@prisma/client";
+import { registerSchema } from "@/schemas/authSchemas";
+
+export async function register(prevState: unknown, formData: FormData) {
+  const validationResult = registerSchema.safeParse({
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    repeatPassword: formData.get("repeatPassword"),
+  });
+
+  if (!validationResult.success) {
+    return {
+      errors: validationResult.error.flatten().fieldErrors,
+    };
+  }
+
+  const { username, email, password } = validationResult.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      error: "Email is already in use",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+      isActive: true,
+      role: Role.USER,
+    },
+  });
+
+  return {
+    success: "User registered",
+  };
+}
