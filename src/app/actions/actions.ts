877b516cd4e8e5e24e624411eccf497e278c6ac2
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2024-12-17 13:42:47 +0100

    Review changes

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,14 +1,13 @@
 "use server";
 import { PrismaClient } from "@prisma/client";
-
+// TODO: change it to not create new instance everytime
 const prisma = new PrismaClient();
 
 export const fetchRecipes = async () => {
   try {
     return prisma.recipe.findMany();
   } catch (error) {
-    //TODO: Add some kind of toast maybe?
-    alert(error);
+    console.log(error);
   } finally {
     prisma.$disconnect;
   }
