f41ed6e8856acf0c3cd6cb9562416ceb8e5dbd74
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2024-12-19 14:24:37 +0100

    Clear timeout on unmount

--- src/hooks/useDebouncedCallback.ts
+++ src/hooks/useDebouncedCallback.ts
@@ -4,7 +4,7 @@ export const useDebouncedCallback = <T extends (...args: any[]) => void>(
   func: T,
   delay: number,
 ) => {
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   const debouncedCallback = useCallback(
     (...args: Parameters<T>) => {
