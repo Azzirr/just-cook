00dcff4589376ecfb4aa680533cd3557e62b6293
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-01-14 23:46:41 +0100

    get bag content dynamically with local storage

--- src/components/ShoppingBag/ShoppingList.tsx
+++ src/components/ShoppingBag/ShoppingList.tsx
@@ -1,28 +1,33 @@
-import { useTranslations } from "next-intl";
+"use client";
+
 import { ShoppingBagIcon } from "lucide-react";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { ShoppingListItem } from "@/components/ShoppingBag/ShoppingListItem";
 
-/* TODO: Remove this placeholder */
-import { placeholderItemsList, emptyList } from "./DELETEMEPlaceholder";
-const itemsList = placeholderItemsList;
-
+// TODO: Fetch actual data from db and bring back ssr and translations
 export const ShoppingBag = () => {
-  const t = useTranslations("ShoppingBag");
+  // const t = useTranslations("ShoppingBag");
+
+  const bagItems: unknown[] = JSON.parse(
+    localStorage.getItem("just-cook-shopping-bag") || "[]",
+  );
 
   return (
     <>
-      {itemsList.length === 0 ? (
+      {bagItems.length === 0 ? (
         <div className="flex flex-col content-center items-center justify-center gap-4 px-2 py-1">
           <ShoppingBagIcon size={100} className="m-4" />
-          <p>{t("isEmpty")}</p>
+          <p>
+            {/* {t("isEmpty")} */}
+            Your shopping bag is empty
+          </p>
         </div>
       ) : (
         <ScrollArea className="px-2 py-1">
           <div className="flex flex-col gap-2">
-            {itemsList.map((item, index) => (
+            {bagItems.map((item, index) => (
               <ShoppingListItem key={index} {...item} />
             ))}
           </div>
