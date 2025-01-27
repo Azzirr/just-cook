3b884a1caad1921e2118daa430a23784903b5a58
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2025-01-27 14:23:36 +0100

    added pwa

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withPWA(nextConfig));
