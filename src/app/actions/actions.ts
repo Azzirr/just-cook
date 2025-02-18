d861799654017d87ca64a902b9312d643d4cbeaa
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2025-02-18 18:04:04 +0100

    feature/added actions for add/remove/show favourite recipes, changed user model, added + icon in recipe

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -8,14 +8,6 @@ export const fetchCategories = async () => {
     console.log(error);
   }
 };
-// TODO - to delete, but I left it for now
-// export const fetchRecipes = async () => {
-//   try {
-//     return db.recipe.findMany();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 
 export const fetchRecipesByCategory = async (categoryId: number) => {
   try {
@@ -26,5 +18,99 @@ export const fetchRecipesByCategory = async (categoryId: number) => {
     });
   } catch (error) {
     console.log("error");
+  }
+};
+
+export const addRecipeToFavourites = async (
+  userId: string,
+  recipeId: number,
+) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: {
+        favouriteRecipes: true,
+      },
+    });
+
+    if (!user) {
+      console.log("User does not exist");
+      return;
+    }
+
+    if (!user.favouriteRecipes.includes(recipeId)) {
+      await db.user.update({
+        where: { id: userId },
+        data: {
+          favouriteRecipes: {
+            push: recipeId,
+          },
+        },
+      });
+      console.log("Recipe added");
+    } else console.log("Recipe is already in favourites");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const removeRecipeFromFavourites = async (
+  userId: string,
+  recipeId: number,
+) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: {
+        favouriteRecipes: true,
+      },
+    });
+
+    if (!user) {
+      console.log("User does not exist");
+      return;
+    }
+
+    if (user.favouriteRecipes.includes(recipeId)) {
+      await db.user.update({
+        where: { id: userId },
+        data: {
+          favouriteRecipes: {
+            set: user.favouriteRecipes.filter((id) => id !== recipeId),
+          },
+        },
+      });
+      console.log("Recipe removed");
+    } else console.log("Error");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getFavouriteRecipes = async (userId: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: {
+        favouriteRecipes: true,
+      },
+    });
+
+    if (!user) {
+      console.log("User does not exist");
+      return [];
+    }
+
+    const favouriteRecipes = await db.recipe.findMany({
+      where: {
+        id: { in: user.favouriteRecipes },
+      },
+    });
+
+    console.log(favouriteRecipes);
+    return favouriteRecipes || [];
+  } catch (error) {
+    console.log(error);
+    return [];
   }
 };
--- src/components/RecipePage/RecipeActions.tsx
+++ src/components/RecipePage/RecipeActions.tsx
@@ -1,17 +1,37 @@
-import { Heart } from "lucide-react";
+"use client";
+import { Heart, PlusCircleIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+
+const recipeActions = [
+  {
+    icon: Heart,
+    action: () => console.log("Added to favorites"),
+    screenReadersOnlyText: "Add recipe to favorites",
+  },
+  {
+    icon: PlusCircleIcon,
+    action: () => console.log("Added to list"),
+    screenReadersOnlyText: "Add recipe to list",
+  },
+];
 
 export const RecipeActions = () => {
   return (
     <section>
-      <Button
-        variant="ghost"
-        className="size-10 rounded-full transition-all duration-150 hover:bg-red-100 hover:text-red-500 focus-visible:bg-red-100 focus-visible:text-red-500 active:scale-110"
-      >
-        <Heart className="size-6" />
-        <span className="sr-only">Add recipe to favorites</span>
-      </Button>
+      {recipeActions.map(
+        ({ icon: Icon, action, screenReadersOnlyText }, index) => (
+          <Button
+            key={index}
+            variant="ghost"
+            onClick={action}
+            className="size-10 rounded-full transition-all duration-150 hover:bg-red-100 hover:text-red-500 focus-visible:bg-red-100 focus-visible:text-red-500 active:scale-110"
+          >
+            <Icon className="size-8" />
+            <span className="sr-only">{screenReadersOnlyText}</span>
+          </Button>
+        ),
+      )}
     </section>
   );
 };
