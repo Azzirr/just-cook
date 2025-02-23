cda41295f4aa56826e9d58d2bcbca846e0c2b12c
Matt <mateuszprozny@MacBook-Air-Mateusz.local>
2025-02-23 17:53:46 +0100

    remove notfoundfallback component and use notfound func

--- src/app/[locale]/category/[category]/[recipe]/page.tsx
+++ src/app/[locale]/category/[category]/[recipe]/page.tsx
@@ -1,6 +1,6 @@
 import { getRecipe } from "@/actions/getRecipe";
-import { NotFoundFallback } from "@/components/NotFoundFallback/NotFoundFallback";
 import { RecipePage } from "@/components/RecipePage/RecipePage";
+import { notFound } from "next/navigation";
 
 type RecipeParams = {
   params: Promise<{
@@ -13,10 +13,7 @@ const recipe = async ({ params }: RecipeParams) => {
 
   const recipe = await getRecipe(Number(recipeId));
 
-  if (!recipe)
-    return (
-      <NotFoundFallback buttonHref="/" buttonLabel="Return to categories" />
-    );
+  if (!recipe) return notFound();
 
   return <RecipePage recipe={recipe} />;
 };
