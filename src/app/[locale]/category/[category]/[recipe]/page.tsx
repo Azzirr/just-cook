be9a0a0862297de636ac3acbc4a6f77b071596e9
Matt <75433152+mproznydev@users.noreply.github.com>
2025-02-26 20:06:20 +0100

    Remove dummy recipe and replace with real data from db  (#46)
    * Remove dummy recipe and fetch real data from db
    
    * add fallback nofound component
    
    * remove notfoundfallback component and use notfound func
    
    * fixes after code review
    
    * fixes after code review
    
    ---------
    
    Co-authored-by: Matt <mateuszprozny@MacBook-Air-Mateusz.local>

--- src/app/[locale]/category/[category]/[recipe]/page.tsx
+++ src/app/[locale]/category/[category]/[recipe]/page.tsx
@@ -1,7 +1,21 @@
+import { getRecipe } from "@/actions/getRecipe";
 import { RecipePage } from "@/components/RecipePage/RecipePage";
+import { notFound } from "next/navigation";
 
-const recipe = () => {
-  return <RecipePage />;
+type RecipeParams = {
+  params: Promise<{
+    recipe: string;
+  }>;
+};
+
+const recipe = async ({ params }: RecipeParams) => {
+  const { recipe: recipeId } = await params;
+
+  const recipe = await getRecipe(Number(recipeId));
+
+  if (!recipe) return notFound();
+
+  return <RecipePage recipe={recipe} />;
 };
 
 export default recipe;
