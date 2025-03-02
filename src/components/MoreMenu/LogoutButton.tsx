eb03deca076974329931e8f9139a69d8d63ddf89
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-02 14:34:12 +0100

    add toast and pending for logout button

--- src/components/MoreMenu/LogoutButton.tsx
+++ src/components/MoreMenu/LogoutButton.tsx
@@ -1,17 +1,33 @@
+"use client";
+
+import { useTransition } from "react";
+import { toast } from "sonner";
 import { LogOut } from "lucide-react";
-import { signOut } from "@/auth";
+
 import { Button } from "@/components/ui/button";
+import { logOut } from "@/actions/logout";
+import { useRouter } from "@/i18n/routing";
 
 export function LogoutButton() {
-  async function logOut() {
-    "use server";
-    await signOut();
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  function handleClick() {
+    startTransition(async () => {
+      const { isSuccess, message } = await logOut();
+      router.refresh();
+      isSuccess ? toast.success(message) : toast.error(message);
+    });
   }
 
   return (
-    <Button className="flex items-center gap-2" onClick={logOut}>
+    <Button
+      className="flex items-center gap-2"
+      onClick={handleClick}
+      disabled={isPending}
+    >
       <LogOut className="size-6" strokeWidth={1.5} />
-      <span>Log Out</span>
+      <span>{isPending ? "Logging Out..." : "Log Out"}</span>
     </Button>
   );
 }
