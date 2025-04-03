d62218e6468aebda91de85c9d4667057ea026d04
mproznydev <mateuszprozny@MacBook-Air-Mateusz.local>
2025-04-03 20:13:56 +0200

    add carousel

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,11 @@
 import { setRequestLocale } from "next-intl/server";
 
-import { Link, type Locale } from "@/i18n/routing";
-import { Button } from "@/components/ui/button";
+import { type Locale } from "@/i18n/routing";
+import { getCategories } from "@/actions/categories/getCategories";
+import SearchBar from "@/components/ui-custom/SearchBar";
+import { CategoriesPreviewCard } from "@/components/Home/CategoriesPreviewCard";
+import { RecipeCarousel } from "@/components/Home/RecipeCarousel";
+import { getRecipes } from "@/actions/recipes/getRecipes";
 
 interface Props {
   params: Promise<{ locale: Locale }>;
@@ -10,20 +14,23 @@ interface Props {
 
 const HomePage = async ({ params, searchParams }: Props) => {
   const { locale } = await params;
-  const { query } = await searchParams;
+  const categories = await getCategories();
+  const recipes = (await getRecipes({ limit: 10 })) || [];
 
   // Enable static rendering
   setRequestLocale(locale);
 
   return (
-    <div className="mx-auto flex max-w-72 flex-col justify-center gap-3 p-4">
-      <h1 className="my-4 text-center">Welcome home</h1>
-      <Button asChild size="lg">
-        <Link href="/categories">Categories</Link>
-      </Button>
-      <Button asChild size="lg">
-        <Link href="/recipes">Recipes</Link>
-      </Button>
+    <div className="flex flex-col gap-8 p-4">
+      <h1 className="mt-3 text-2xl">What do you want to cook today?</h1>
+      <div className="max-w-[400px]">
+        <SearchBar></SearchBar>
+      </div>
+      <CategoriesPreviewCard categories={categories}></CategoriesPreviewCard>
+      <div className="pt-[40px]">
+        <p className="pb-[20px] text-xl">Recently added recipes</p>
+        <RecipeCarousel recipes={recipes}></RecipeCarousel>
+      </div>
     </div>
   );
 };
--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -22,10 +22,12 @@ export const BaseLayout = ({ children, locale }: Props) => {
   return (
     <html lang={locale}>
       <body
-        className={`antialiased ${roboto.className} flex min-h-screen flex-col`}
+        className={`antialiased ${roboto.className} flex min-h-screen flex-col bg-[#f8f8f8]`}
       >
         <TopNavigation />
-        <div className="flex grow flex-col">{children}</div>
+        <div className="mx-auto flex w-full max-w-[1300px] grow flex-col">
+          {children}
+        </div>
         <BottomNavigation />
         <Toaster />
       </body>
