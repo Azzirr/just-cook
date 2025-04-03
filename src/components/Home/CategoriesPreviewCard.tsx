cd31605fea8838fe8c6068eeced8894af0069acd
mproznydev <mateuszprozny@MacBook-Air-Mateusz.local>
2025-04-03 20:26:06 +0200

    remove old code

--- src/components/Home/CategoriesPreviewCard.tsx
+++ src/components/Home/CategoriesPreviewCard.tsx
@@ -10,22 +10,20 @@ export const CategoriesPreviewCard = ({
     "https://res.cloudinary.com/dqpm8hjgz/image/upload/c_thumb,w_200,g_face/v1743697684/more_eaezkq.png";
 
   return (
-    <div className="relative">
-      <div className="relative z-[10] grid grid-cols-[repeat(auto-fill,_minmax(170px,_1fr))] gap-5 rounded-xl bg-[#ffffff] p-4 shadow-md">
-        {categories.map((category) => (
-          <CategoriesPreviewElement
-            key={category.id}
-            label={category.name}
-            href={`/categories/${category.slug}`}
-            icon={category.icon}
-          ></CategoriesPreviewElement>
-        ))}
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(170px,_1fr))] gap-5 rounded-xl bg-[#ffffff] p-4 shadow-md">
+      {categories.map((category) => (
         <CategoriesPreviewElement
-          label={"See more"}
-          href={`/categories`}
-          icon={SEE_MORE_ICON}
+          key={category.id}
+          label={category.name}
+          href={`/categories/${category.slug}`}
+          icon={category.icon}
         ></CategoriesPreviewElement>
-      </div>
+      ))}
+      <CategoriesPreviewElement
+        label={"See more"}
+        href={`/categories`}
+        icon={SEE_MORE_ICON}
+      ></CategoriesPreviewElement>
     </div>
   );
 };
