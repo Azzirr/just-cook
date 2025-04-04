69a8c5bf7add729758c1b99412b80114ee8ae6a7
mproznydev <mateuszprozny@MacBook-Air-Mateusz.local>
2025-04-04 20:59:34 +0200

    add logo

--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { CircleUserRound, ShoppingBag } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 
-import ExampleImage from "@/public/images/example-image.png";
+import Logo from "@/public/images/logo-transparent.png";
 import { Link } from "@/i18n/routing";
 import { currentSession } from "@/lib/currentSession";
 import { getUserById } from "@/data/user";
@@ -15,7 +15,7 @@ export const TopNavigation = async () => {
   return (
     <div className="sticky top-0 z-10 flex w-full items-center justify-between bg-slate-100 px-3 py-1">
       <Link href="/">
-        <Image src={ExampleImage} alt="Example description" className="w-32" />
+        <Image src={Logo} alt="Example description" className="w-16" />
       </Link>
 
       <div className="flex items-center gap-3">
