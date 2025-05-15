4962af9cd09fb9d4e317de15752e0193f57cb5cb
Matt <75433152+mproznydev@users.noreply.github.com>
2025-05-15 07:56:56 +0200

    Add comment section (#97)
    * Add comment section
    
    * hide comment form for non log in
    
    * fixes after code review
    
    * improve grid
    
    ---------
    
    Co-authored-by: mproznydev <mateuszprozny@MacBook-Air-Mateusz.local>

---
+++ src/components/RecipeDetail/RecipeComment.tsx
@@ -0,0 +1,26 @@
+import { RecipeComment as RecipeCommentPrisma, User } from "@prisma/client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { formatToShortDate } from "@/utils/formatToShortDate";
+import { getUsernameInitials } from "@/utils/getUsernameInitials";
+
+type RecipeCommentProps = { comment: RecipeCommentPrisma & { author: User } };
+
+export const RecipeComment = async ({ comment }: RecipeCommentProps) => {
+  const formatDate = await formatToShortDate();
+
+  const { author, text, createdAt } = comment;
+  return (
+    <div className="grid grid-cols-[auto_1fr] gap-2">
+      <Avatar className="h-8 w-8 bg-slate-50">
+        <AvatarImage src={author.avatar ?? undefined} />
+        <AvatarFallback>{getUsernameInitials(author.username)}</AvatarFallback>
+      </Avatar>
+      <div>
+        <p className="font-bold">{author.username}</p>
+        <p className="text-xs">{formatDate(createdAt)}</p>
+      </div>
+      <p className="col-start-2">{text}</p>
+    </div>
+  );
+};
